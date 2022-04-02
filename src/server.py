be07b4d4d184ad0f9806ac8e8f66ecde5fa084b5
Ixesha <terry1651140@gmail.com>
2022-04-02 19:31:50 +0800

    Camera interaction

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 import os.path, sys
+import base64
 CENTERNET_PATH = './lib/'
 sys.path.insert(0, CENTERNET_PATH)
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import FileResponse
 from detectors.detector_factory import detector_factory
 from opts import opts
@@ -30,6 +31,20 @@ TASK = 'ddd'
 opt = opts().init('{} --load_model {}'.format(TASK, MODEL_PATH).split(' '))
 detector = detector_factory[opt.task](opt)
 
+@app.post("/camera/uploadcamera/")
+async def creat_camera_img(request: Request):
+    payload = await request.json()
+    camera_b64 = payload['camera']
+    nparr = np.fromstring(base64.b64decode(camera_b64.split(',')[1]), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    ret = detector.run(img)['results']
+    detector.show_results(Debugger(dataset=opt.dataset), img, ret)
+    img_ID = 0
+    if os.path.exists("../../outputCenterNet/id.txt"):
+    	img_ID = int(open("../../outputCenterNet/id.txt", "r").read()) - 1
+    response_img = "../../outputCenterNet/{}add_pred.png".format(img_ID)
+    return FileResponse(response_img)
+
 @app.get("/service/getPhotosNumber/")
 def get_photos_number():
     if os.path.exists("../../outputCenterNet/id.txt"):
@@ -45,6 +60,7 @@ def read_photos(photo_id: int):
 
 @app.post("/service/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    print(file)
     img = await file.read()
     img_buffer = np.frombuffer(img, dtype=np.uint8)
     img_numpy = cv2.imdecode(img_buffer, 1)
